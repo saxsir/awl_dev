5b6ed60c4bf923cb4d04191c1e189cd7602ba1fc
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-01-08 19:09:03 +0900

    rails new awl_dev

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwlDev::Application.config.secret_token = 'e779dbd050384930fdf0fba435c51ca39ac44d1f918b6ff1c8f7306bed6f72c12997068a3a81cacc67afcd2fe224c8f9ad6f861751a9e7cb2db5395aa53eb3cd'
