ef5024730f79375935c53144cc96e83642b03c43
Sho Nakazono <nikezono@gmail.com>
2013-01-11 22:22:02 +0900

    rails g migration AddArtistToUsers artist:booleanした

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130111123151) do
+ActiveRecord::Schema.define(:version => 20130111132025) do
 
   create_table "order_transactions", :force => true do |t|
     t.integer  "order_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20130111123151) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "artist"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
