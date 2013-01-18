932a7f8a0a0173a1d5361efc22dec94c58610d44
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-01-18 20:22:18 +0900

    signupの際にusernameでバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+  devise :database_authenticatable, :registerable, :authentication_keys => [:username]
+  devise :recoverable, :rememberable, :trackable, :validatable
+  
+  validates :username,:presence=>true,:length=>{:maximum => 20}
+  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :artist, :username
   # attr_accessible :title, :body
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,6 +25,9 @@ Devise.setup do |config|
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [ :email ]
 
+  # add
+  # config.authentication_keys = [ :username, :email ]
+  
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,
