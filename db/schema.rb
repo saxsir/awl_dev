2d755f92ce49d437187293881fa1355efd3b64be
Sho Nakazono <nikezono@gmail.com>
2013-02-13 15:30:22 +0900

    jmpress関連をreset&.gitignoreを更新&seed.rbを修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130211100416) do
+ActiveRecord::Schema.define(:version => 20130211143403) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",               :default => "", :null => false
