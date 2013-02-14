15a96b02baa2cd1329d432361588eb08b1d3c96e
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-02-14 14:26:46 +0900

    modelでprojectとrewardを関連付けした
    project has many rewards
    reward belongs to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :target_amount, :title, :total_amount, :url, :user_id, :payment_tag, :image_url, :headline, :slideshow
   belongs_to :user
+  has_many :rewards
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,4 @@
 class Reward < ActiveRecord::Base
   attr_accessible :description, :project_id, :title
+  belongs_to :project
 end
