6db8ed9120af41a42131fce7ee98b55be67f9726
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-02-15 02:45:52 +0900

    home/indexのマークアップとスタイリング
    home/indexだけ使うレイアウトファイルを分けた
    application.css内でのassets以下のcssの自動読み込みを削除
    home/indexでのみhome.css.scssを読み込むようにした
    home/indexのheader-top（上のやつ）は固定。レスポンシブ未対応。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   # GET /
   def index
     @projects = Project.find([1,2,3,4,5])
-    respond_to do |format|
-      format.html # index.haml
-    end
+    render :layout => 'home_template'
+  end
+
+  # GET /about
+  def about
   end
 end
