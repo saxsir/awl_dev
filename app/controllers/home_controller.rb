50d712b3546f7ce0e7766df2c4ef4046816501ed
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-02-16 01:39:35 +0900

    topページのマークアップ微修正
    application.hamlを共通テンプレートにし、home/index.hamlの中でheaderのなかにcover（navbarの上）をいれるようにした
    home_controller内でテンプレートファイルをapplication.hamlに戻した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   # GET /
   def index
     @projects = Project.find([1,2,3,4,5])
-    render :layout => 'home_template'
+    respond_to do |format|
+        format.html # index.html.erb
+      end
   end
 
   # GET /about
