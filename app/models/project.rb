395c9c27f14e7de93026c96f3276b10a7c0f9f06
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-02-19 00:23:08 +0900

    モデルの修正
    プロジェクトとサポートの関連付け
    サポートとユーザーの関連付け

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
-  attr_accessible :target_amount, :title, :total_amount, :url, :user_id, :payment_tag, :image_url, :headline, :slideshow
+  attr_accessible :support_id, :target_amount, :title, :total_amount, :url, :user_id, :payment_tag, :image_url, :headline, :slideshow
   belongs_to :user
   has_many :rewards
+  has_many :supports
 end
