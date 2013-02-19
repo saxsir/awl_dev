1156eed7dc0d54cd8f95032e05969ae0e9a9ffb5
Sho Nakazono <nikezono@gmail.com>
2013-02-19 23:36:33 +0900

    全ページに共通のpartialとしてflash-messageを導入
    ・詳細はhttp://ruby.zigzo.com/2011/10/02/flash-messages-twitters-bootstrap-css-framework/
    ・flash-messageを受け取る:noticeとかはもう書かなくて良い

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	#http://ruby.zigzo.com/2011/10/02/flash-messages-twitters-bootstrap-css-framework/
+	#を参考にしてflash-messageのtypeを設定する
+	def twitterized_type(type)
+	  case type
+	    when :alert
+	      "alert-block"
+	    when :error
+	      "alert-error"
+	    when :notice
+	      "alert-info"
+	    when :success
+	      "alert-success"
+	    else
+	      type.to_s
+	  end
+	end
+
 end
