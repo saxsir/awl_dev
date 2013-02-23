bed8fde0389448c2e359be2af52998ec0098f24d
Sho Nakazono <nikezono@gmail.com>
2013-02-23 14:31:23 +0900

    とりあえずpaypal関連をviewに反映させてた部分は消した

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -24,7 +24,7 @@ class SupportsController < ApplicationController
       if @support.save
       	#うまく行ってたらparent(プロジェクトページ)にnoticeつきで飛ばす
       	#postからpostへリダイレクトは出来ない。よってボタン増やしたのでここでは何も行わない
-      	#成功時とエラー時にそれぞれnoticeを送るようにしたい。つうかバリデーション
+       	#成功時とエラー時にそれぞれnoticeを送るようにしたい。つうかバリデーション
       else
         #format.html { render action: "new" }
         #format.json { render json: @support.errors, status: :unprocessable_entity }
