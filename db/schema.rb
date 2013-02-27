8c14a9b32871425f953b68dedc091b1fb936e5ce
Sho Nakazono <nikezono@gmail.com>
2013-02-27 20:47:32 +0900

    migrateしたら勝手に変更されただけ

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130220144156) do
+ActiveRecord::Schema.define(:version => 20130227092044) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",               :default => "", :null => false
@@ -27,6 +27,20 @@ ActiveRecord::Schema.define(:version => 20130220144156) do
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+
+  create_table "payments", :force => true do |t|
+    t.integer  "amount",     :default => 1
+    t.string   "token"
+    t.string   "identifier"
+    t.string   "payer_id"
+    t.boolean  "recurring",  :default => false
+    t.boolean  "digital",    :default => false
+    t.boolean  "popup",      :default => false
+    t.boolean  "completed",  :default => false
+    t.boolean  "canceled",   :default => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -76,6 +90,11 @@ ActiveRecord::Schema.define(:version => 20130220144156) do
     t.datetime "updated_at", :null => false
     t.string   "comment"
     t.boolean  "success"
+    t.string   "token"
+    t.string   "payer_id"
+    t.boolean  "canceled"
+    t.boolean  "completed"
+    t.string   "identifier"
   end
 
   create_table "users", :force => true do |t|
