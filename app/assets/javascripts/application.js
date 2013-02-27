5a87509aec178abe68046b13f79afa5e62ef0bd7
Sho Nakazono <nikezono@gmail.com>
2013-02-28 03:08:52 +0900

    Sign_inフォームがpopoverになるようにした
    ・jsでcontentにtextを入れるようにした（xssの危険があるらしいのでテストを後でしたい)
    ・微妙にnavbarの並びを変えた

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,17 @@
 //= require jtruncsubstr/jquery.jtruncsubstr-1.0rc.min.js
 //= require jquery-easing/jquery.easing.1.3.js
 //= require jquery.numeric/jquery.numeric.js
+
+var form_tag = '<form accept-charset="UTF-8" action="/users/sign_in" class="new_user" id="new_user" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><label for="user_login">Login</label><input class="span2" id="user_login" name="user[login]" size="15" type="text" /><label for="user_password">Password</label><input class="span2" id="user_password" name="user[password]" size="15" type="password" /><br><input name="user[remember_me]" type="hidden" value="0" /><input type = "hidden"id="user_remember_me" name="user[remember_me]" type="checkbox" value="1" checked /><input class="btn btn-primary pull-right" name="commit" type="submit" value="Sign in" /></form>';
+
+$(function(){
+
+	$("#nav_login").popover({
+		html:true,
+		offset: 5,
+		placement: 'bottom',
+		title: "ログイン",
+		content: form_tag
+	});
+	$('#nav_login').die('mouseleave');
+});
